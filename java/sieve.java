5a65836169cdfdda8dfc1551a0a259e6aacb621b
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2021-01-06 15:50:43 -0800

    Update

--- java/sieve.java
+++ java/sieve.java
@@ -1,13 +1,13 @@
 // sieve of eratosthenes
 
 // time complexity: O(n log log n)
-// sieve(n) returns an array div, where div[i] = 1 if i is prime otherwise a prime divisor of i, for i up to n
+// sieve(n) returns an array div, where div[i] = a prime divisor of i for i in [2, n]
 
 static int[] sieve(int n) {
     int[] div = new int[n + 1];
     for (int i = 2; i <= n; ++i) {
         if (div[i] == 0) {
-            div[i] = 1;
+            div[i] = i;
             if ((long) i * i <= n) {
                 int j = i * i;
                 while (j <= n) {
